a2548aa93add2220e8019da77dc423a5a3fa4ebe
zengzhipeng <zengzhipeng1207@163.com>
2019-08-20 10:58:09 +0800

    学生实体类

---
+++ src/main/java/pub/zengzhipeng/entity/Student.java
@@ -0,0 +1,17 @@
+package pub.zengzhipeng.entity;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+import java.io.Serializable;
+
+/**
+ * @Description
+ * @Author zzp
+ * @Date 2019/8/20 10:56
+ * @Version 1.0
+ **/
+public class Student implements Serializable {
+
+    private String name;
+    private int age;
+}
