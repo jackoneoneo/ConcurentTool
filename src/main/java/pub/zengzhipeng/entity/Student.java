b795cb75efb5f71b814d58c3c5a095c2d4f715ed
zengzhipeng <zengzhipeng1207@163.com>
2019-08-23 09:09:50 +0800

    添加了Comparable的使用案例

--- src/main/java/pub/zengzhipeng/entity/Student.java
+++ src/main/java/pub/zengzhipeng/entity/Student.java
@@ -4,13 +4,40 @@ package pub.zengzhipeng.entity;
 import java.io.Serializable;
 
 /**
- * @Description
+ * @Description Comparable 的示例，实现Comparable 接口的compareTo方法
+ * 该方法返回3个值，整数、零、负数
  * @Author zzp
  * @Date 2019/8/20 10:56
  * @Version 1.0
  **/
-public class Student implements Serializable {
+public class Student implements Serializable ,Comparable<Student>{
 
     private String name;
     private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int compareTo(Student student) {
+        if(this.age == student.getAge()){
+            return 0;
+        }else if(this.age > student.getAge()){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
 }
