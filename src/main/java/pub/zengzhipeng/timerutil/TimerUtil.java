07e967ad21e337cb230e191dd356b4a4c726d653
zengzhipeng <zengzhipeng1207@163.com>
2019-08-26 10:49:06 +0800

    Timer的使用案例

--- src/main/java/pub/zengzhipeng/timerutil/TimerUtil.java
+++ src/main/java/pub/zengzhipeng/timerutil/TimerUtil.java
@@ -1,7 +1,12 @@
 package pub.zengzhipeng.timerutil;
 
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServlet;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description Timer 类的使用方法，
@@ -11,6 +16,8 @@ import java.util.TimerTask;
  **/
 public class TimerUtil {
     public static void main(String[] args) {
+
+        disp(TimeUnit.DAYS);
         Timer timer = new Timer("timer 管理器");
         timer.schedule(new TimerTask() {
             @Override
@@ -26,5 +33,11 @@ public class TimerUtil {
         },2000);
 
 
+
+
+    }
+    public static void disp(TimeUnit unit){
+
+        System.out.println(unit.toHours(2));
     }
 }
